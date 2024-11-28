f1c8ca146ceb4dfbd414a8b9140105144f4d4ec8
Sebastien MAILLET <sebastien.maillet@gmail.com>
2024-11-28 13:26:49 +0100

    fix:tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./assets/**/*.js",
+    "./templates/**/*.html.twig",
+    "./src/**/*.{html,js}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
